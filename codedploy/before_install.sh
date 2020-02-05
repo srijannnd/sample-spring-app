f692c8d7a9f93315d9cbf333dfa035a9e5dd5dbd
srijannnd <srijan.pydev@gmail.com>
2020-02-05 17:29:02 +0530

    before removed

--- codedploy/before_install.sh
+++
@@ -1,7 +0,0 @@
-yum install -y java-1.8.0-openjdk-devel
-yum remove java-1.7.0-openjdk
-wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
-sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-yum install -y apache-maven
-mvn --version
-yum install -y git
