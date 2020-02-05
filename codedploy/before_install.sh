f6eca3921dfddc4a28fa4c0899984ba367869aee
srijannnd <srijan.pydev@gmail.com>
2020-02-05 17:23:36 +0530

    sh file modified

--- codedploy/before_install.sh
+++ codedploy/before_install.sh
@@ -4,4 +4,4 @@ wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.re
 sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
 yum install -y apache-maven
 mvn --version
-yum install -y git 
+yum install -y git
