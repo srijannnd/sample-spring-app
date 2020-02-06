8d6715e56418943d6182fa7129ef8854e7c269d8
srijannnd <srijan.pydev@gmail.com>
2020-02-06 17:24:43 +0530

    script modified

--- codedeploy/before_install.sh
+++ codedeploy/before_install.sh
@@ -1,3 +1,6 @@
+# install updates
+yum update -y
+
 yum install -y java-1.8.0-openjdk-devel
 yum remove java-1.7.0-openjdk
 wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -1,39 +1,40 @@
-#nohup java8 -jar /tmp/target/gs-spring-boot-docker-0.1.0.jar --server.port=8080 > output.log &
+nohup java8 -jar /tmp/target/gs-spring-boot-docker-0.1.0.jar --server.port=8080 > output.log &
 
 #!/bin/bash
-# install updates
-yum update -y
 
 # install apache httpd
-yum install httpd -y
+#yum install httpd -y
 
 # create the working directory
-mkdir /opt/spring-boot-ec2-demo
+#mkdir /opt/spring-boot-ec2-demo
 # create configuration specifying the used profile
-echo "RUN_ARGS=--spring.profiles.active=ec2" > /opt/spring-boot-ec2-demo/spring-boot-ec2-demo.conf
+#echo "RUN_ARGS=--spring.profiles.active=ec2" > /opt/spring-boot-ec2-demo/spring-boot-ec2-demo.conf
 
 # create a springboot user to run the app as a service
-useradd springboot
+#useradd springboot
 # springboot login shell disabled
-chsh -s /sbin/nologin springboot
-chown springboot:springboot /tmp/target/gs-spring-boot-docker-0.1.0.jar
-chmod 500 /tmp/target/gs-spring-boot-docker-0.1.0.jar
+#chsh -s /sbin/nologin springboot
+#chown springboot:springboot /tmp/target/gs-spring-boot-docker-0.1.0.jar
+#chmod 500 /tmp/target/gs-spring-boot-docker-0.1.0.jar
 
 # create script
-rm /etc/init.d/spring-boot-ec2-demo
-echo "java8 -jar /tmp/target/gs-spring-boot-docker-0.1.0.jar" >> /etc/init.d/spring-boot-ec2-demo
+#rm /etc/init.d/spring-boot-ec2-demo
+#echo "java8 -jar /tmp/target/gs-spring-boot-docker-0.1.0.jar" >> /etc/init.d/spring-boot-ec2-demo
+
+# Give Permission to open app config
+#chmod +x /etc/init.d/spring-boot-ec2-demo
 
 # forward port 80 to 8080
-echo "<VirtualHost *:80>
-ProxyRequests Off
-ProxyPass / http://localhost:8080/
-ProxyPassReverse / http://localhost:8080/
-</VirtualHost>" >> /etc/httpd/conf/httpd.conf
+#echo "<VirtualHost *:80>
+#ProxyRequests Off
+#ProxyPass / http://localhost:8080/
+#ProxyPassReverse / http://localhost:8080/
+#</VirtualHost>" >> /etc/httpd/conf/httpd.conf
 
 # start the httpd and spring-boot-ec2-demo
-service httpd start
-service spring-boot-ec2-demo start
+#service httpd start
+#service spring-boot-ec2-demo start
 
 # automatically start httpd and spring-boot-ec2-demo if this ec2 instance reboots
-chkconfig httpd on
-chkconfig spring-boot-ec2-demo on
+#chkconfig httpd on
+#chkconfig spring-boot-ec2-demo on
