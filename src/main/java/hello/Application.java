7aff1c043aac134db5cc098e5cc7ea492121322a
srijannnd <srijan.pydev@gmail.com>
2020-02-07 12:57:18 +0530

    text modified

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
+		return "Hello Spring World";
 	}
 
 	public static void main(String[] args) {
