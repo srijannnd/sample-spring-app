e6498ef00685bc105cb041b23b721000d0642056
srijannnd <srijan.pydev@gmail.com>
2020-03-10 22:10:20 +0530

    init

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,21 +1,23 @@
-package hello;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 @RestController
-public class Application {
-
+public class DemoApplication {
+	
 	@RequestMapping("/")
 	public String home() {
 		return "Hello Spring World!!!!!";
 	}
 
+
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
